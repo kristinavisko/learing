75a908882e5a47d61f9307b4313efe4723c92951
kristinavisko <37777417+kristinavisko@users.noreply.github.com>
2018-11-14 16:48:13 +0100

    Update zadatak1.py

---
+++ zadatak1.py
@@ -0,0 +1,19 @@
+"""Unesite 3 broja. Ukoliko su svi veci od 10, ispisite njihov zbroj. U suprotnom ispitajte koji je od njih najveci pa ga pomnozite sa samim sobom te nakon toga ispisite njihov zbroj."""
+
+x = 2
+y = 15
+z = 12
+
+if x > 10 and y > 10 and y > 10:
+    suma = x + y + z
+    print suma
+else: 
+    if x >= z and x >= z:
+        big = x*x
+        print big + y + z
+    elif y >= x and y >= z:
+        big = y*y
+        print big + x + z
+    else:
+        big = z*z
+        print big + y + x
