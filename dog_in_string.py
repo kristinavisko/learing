2787d4b4417488789fd04cdf99eba03cf49e3022
Kristina Viskovic <kristina.visko@gmail.com>
2018-11-24 14:22:45 +0100

    New exercise with dog in string

---
+++ dog_in_string.py
@@ -0,0 +1,12 @@
+"""
+Find if the word "dog" is in string.
+"""
+
+def dog_in_string(word):
+    return "dog" in word.lower()
+    
+
+print (dog_in_string("-Some string with Dog-"))
+
+        
+            
